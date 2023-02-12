a763cf902cac81b318828bca5d4707ad1ebd7af0
RRaihan123 <ronyraihan141@gmail.com>
2023-02-12 09:04:15 +0600

    initial commit

---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  constructor(private http : HttpClient) { }
+
+  postEmployee(data:any){
+    return this.http.post("http://localhost:3000/posts",data)
+    }
+  getEmployee(){
+    return this.http.get("http://localhost:3000/posts")
+   }
+  updateEmployee(data:any,id:number){
+    return this.http.put("http://localhost:3000/posts/"+id,data)
+  }
+  deleteEmployee(id:number){
+    return this.http.delete("http://localhost:3000/posts/"+id)
+  }
+}
